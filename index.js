5c17465548c10962ae30316642cf20f57e781660
mdsujon428 <mdsujon000056@outlook.com>
2021-10-27 11:16:03 +0600

    processed.env.PORT || 5000 added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 const app = express();
 // const port = process.env.PORT || 5000;
-const port = 5000;
+const port =process.env.PORT || 5000;
 
 //middleWare
 app.use(cors())
